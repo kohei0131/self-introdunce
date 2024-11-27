912654de163f84ad87a446b20d758423f4e7b9bb
c230193 <c230193@hsakuragaoka-h.ed.jp>
2024-11-27 02:54:03 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+function checkEven0r0ddproduct(a,b)
+ア絵rjjふぇじえじぇjrj3えっじぇrjfjvっじゃウェイjf
+
+
+
+const port = parseInt(process.env.PORT || '3000');
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
